d898e6e74f4d106e79346025c102dec6328999ea
Linnea Liesén <linnealiesen@protonmail.com>
2021-09-02 19:30:00 +0200

    added App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CardList from './CardList';
+import { robots } from './robots.js';
+
+const App = () => {
+	return (
+		<CardList robots={robots} />
+	);
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-import CardList from './CardList';
-import { robots } from './robots.js';
 import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <CardList robots={robots} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
